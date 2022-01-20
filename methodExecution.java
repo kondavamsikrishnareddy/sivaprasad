83150fc45bde9816c75b4514b1eade47041eec8a
kondavamsikrishnareddy <97826850+kondavamsikrishnareddy@users.noreply.github.com>
2022-01-20 10:37:48 +0530

    Add files via upload

---
+++ methodExecution.java
@@ -0,0 +1,15 @@
+public class methodExecution {
+
+		//method demo
+		public int multipynumbers(int a,int b) {
+			int z=a*b;
+			return z;
+		}
+
+		public static void main(String[] args) {
+
+			methodExecution b=new methodExecution();
+			int ans= b.multipynumbers(25,7);
+			System.out.println("Multipilcation is :"+ans);
+			}
+		}
