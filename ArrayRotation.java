648c7a65e17bc339c08b3de8d7083d00258202ef
kondavamsikrishnareddy <97826850+kondavamsikrishnareddy@users.noreply.github.com>
2022-01-27 22:20:02 +0530

    Add files via upload

---
+++ ArrayRotation.java
@@ -0,0 +1,31 @@
+public class ArrayRotation
+ class RotateArray { 
+public void rotate(int[] nums, int k) {
+    		if(k > nums.length) 
+       			k=k%nums.length;
+ 		int[] result = new int[nums.length];
+ 		for(int i=0; i < k; i++){
+        			result[i] = nums[nums.length-k+i];
+ 		}
+ 		int j=0;
+    		for(int i=k; i<nums.length; i++){
+        			result[i] = nums[j];
+j++;
+    		}
+ 		System.arraycopy( result, 0, nums, 0, nums.length );
+}
+} 
+
+
+	public static void main(String[] args) {
+		
+		RotateArray r = new RotateArray();
+		int arr[] = { 8, 9, 10, 11, 12, 13, 14 }; 
+		r.rotate(arr, 5); 
+		for(int i=0;i<arr.length;i++){
+    			System.out.print(arr[i]+" ");
+
+	 }
+
+   }
+}
