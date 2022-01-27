e917993b7064d1261b106368bc99aebdc65734d9
kondavamsikrishnareddy <97826850+kondavamsikrishnareddy@users.noreply.github.com>
2022-01-27 22:31:53 +0530

    Add files via upload

---
+++ Binarysearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch {
+
+	public static void main(String[] args) {
+        int[] arr = {1,5,10,15,20};
+        int key = 15;
+        int arrlength = arr.length;
+        binarySearch(arr,0,key,arrlength);
+    }
+
+public static void binarySearch(int[] arr, int start, int key, int length){
+
+        int midValue = (start+length)/2;
+        while(start<=length){
+
+            if(arr[midValue]<key){
+
+                start = midValue + 1;
+            } else if(arr[midValue]==key){
+                System.out.println("Element is found at index :"+midValue);
+                break;
+            }else {
+
+                length=midValue-1;
+            }
+            midValue = (start+length)/2;
+        }
+            if(start>length){
+
+                System.out.println("Element is not found");
+            }
+
+
+	}
+
+}
